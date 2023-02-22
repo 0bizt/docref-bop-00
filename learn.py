b89b668cb2aa9444feed9c5bdd81bfc88fcee733
jerry <jerrynwosu007@gmail.com>
2023-02-22 14:35:50 +0100

    learn stuff commit msg

---
+++ learn.py
@@ -0,0 +1,6 @@
+age = 20
+name = 'Swaroop'
+
+print(f'{name} was {age} years old when he wrote this book', end="\n")
+print(f'why is {name} playing with that python?')
+print(r"Newlines are indicated by\n Jus so you know")
